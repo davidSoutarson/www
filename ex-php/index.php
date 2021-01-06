203c1b044aafc8d18b4d324f892839fa372ca118
david soutarson <triplaxs@gmail.com>
2021-01-06 09:52:29 +0100

    une metho pour aprendre

---
+++ ex-php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>le-php-tester-pas-a-pas</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li> <a href="#"> Les Variable exemple utilisation du pHP</a> </li>
+          <li> <a href="#"> Les conditions et les boucle exemple utilisation du pHP</a> </li>
+          <li> <a href="#"> Les tablaux exemple utilisation du pHP</a> </li>
+          <li> <a href="#"> Les Varible pres defini et Varible externe exemple utilisation du pHP</a> </li>
+          <li> <a href="#"> Les Formulaires exemple utilisation du pHP</a> </li>
+          <li> <a href="#"> Les exemple utilisation du pHP</a> </li>
+        </ul>
+      </nav>
+    </header>
+
+  </body>
+</html>
