1707188c698d4015b9b4a53044e128b87f6bcff6
david soutarson <triplaxs@gmail.com>
2021-01-14 00:59:35 +0100

    devoir 1 php 13 01 2021

---
+++ devoir01php/formulaire.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>fomulaire covoiturage</title>
+  </head>
+  <body>
+
+    <main>
+      <form class="fomulaire" action="resulta.php" method="post">
+
+        <p> Nom <input type="text" name="nom" value=""> </p>
+        <p> Prénom <input type="text" name="premon" value=""> </p>
+        <p> Adresse email <input type="text" name="email" value=""> </p>
+        <p> Numéro de téléphone  <input type="text" name="telephone" value=""> </p>
+        <p> propauser vaure covoiturage </p>
+        <p> Sélection de la ville de départ <input type="text" name="villeDepart" value=" Ville de Depart"> Heure de Depart:<input type="text" name="departTime" value="Heure de Depart"> </p>
+        <p> Sélection de la ville de ariver <input type="text" name="villeAriver" value=" Ville d'Ariver "> Heure d'ariver :<input type="text" name="ariverTime" value="Heure d'Ariver "> </p>
+
+
+      </form>
+
+    </main>
+
+  </body>
+</html>
