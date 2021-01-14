8e0d09c13f235f6a4112f13ee6232de8aa8c3611
david soutarson <triplaxs@gmail.com>
2021-01-14 15:06:20 +0100

    v2 une api plus aboutie nise en form ajou de bouton d'envoie

--- devoir01php/formulaire.php
+++ devoir01php/formulaire.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>fomulaire covoiturage</title>
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 
@@ -13,24 +14,25 @@
 
         <p>
             <label for = "nom">Entre votre nom : </label>
-            <input type="text" name="nom" value="">
+            <input class="chamInfo" type="text" name="nom" value="">
          </p>
         <p>
             <label for = "premon">Entre votre prénom : </label>
-            <input type="text" name="prenom" value="">
+            <input class="chamInfo" type="text" name="prenom" value="">
         </p>
         <p>
-            <label for="email">Adresse email</label>
-            <input type="text" name="email" value="">
+            <label for="email">Adresse email : </label>
+            <input class="chamInfo" type="text" name="email" value="">
          </p>
         <p>
-            <label for="telephone">Numéro de téléphone</label>
-            <input type="text" name="telephone" value="">
+            <label for="telephone">Numéro de téléphone : </label>
+            <input class="chamInfo" type="text" name="telephone" value="">
         </p>
         <p> propauser vaure covoiturage </p>
-        <p> Sélection de la ville de départ <input type="text" name="villeDepart" value=" Ville de Depart"> Heure de Depart:<input type="text" name="departTime" value="Heure de Depart"> </p>
-        <p> Sélection de la ville de ariver <input type="text" name="villeAriver" value=" Ville d'Ariver "> Heure d'ariver :<input type="text" name="ariverTime" value="Heure d'Ariver "> </p>
+        <p> Sélection de la ville de départ <input class="chamVile" type="text" name="villeDepart" value=" Ville de Depart"> Heure de Depart:<input type="text" name="departTime" value="Heure de Depart"> </p>
+        <p> Sélection de la ville de ariver <input class="cham chamVile" type="text" name="villeAriver" value=" Ville d'Ariver "> Heure d'ariver :<input type="text" name="ariverTime" value="Heure d'Ariver "> </p>
 
+        <input class="valider" type="submit" name="validation" value="valider">
 
       </form>
 
