02c3afcfc11e9b5ce6e6bb007fad63520394f649
david soutarson <triplaxs@gmail.com>
2021-01-24 12:11:37 +0100

    logo

--- PHP-devoire01/vue-api-php/header.php
+++ PHP-devoire01/vue-api-php/header.php
@@ -2,7 +2,7 @@
 
   <h1>COVOITURAGE</h1>
   <div class="logo">
-    <img src="logo.jpg" alt="inmage sevent de logo a l'aplication">
+    <img src="img/logo-covoit.jpg" alt="inmage sevent de logo a l'aplication">
   </div>
 
   <nav>
