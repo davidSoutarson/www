12715d87aea42fa5dc6a95b9cf71754be7e0ebc6
david soutarson <triplaxs@gmail.com>
2021-01-29 00:21:47 +0100

    aprendre le sql

---
+++ premier-pas-sql/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Apprendre  My squl</title>
+  </head>
+  <body>
+
+    <form class="" action="index.html" method="post">
+
+      <p>Un formulaire daprentisage type utilisateur</p>
+
+      <p><label for="nom">votre nom : </label> </p>
+      <p><input type="text" name="nom" value="" placeholder="nom"> </p>
+
+      <p><label for="prenom">voter prenom : </label> </p>
+      <p><input type="text" name="Prenom" value="" placeholder="prenom"> </p>
+
+      <p><label for="mail">votre mail : </label> </p>
+      <p><input type="text" name="mail" value="" placeholder="mail@"> </p>
+
+
+    </form>
+
+  </body>
+</html>
