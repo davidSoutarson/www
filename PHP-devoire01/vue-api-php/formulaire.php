4994e92e7212844dab7cc995562d7e5bd5fdded4
david soutarson <triplaxs@gmail.com>
2021-02-01 13:16:08 +0100

    devoire reprise

--- PHP-devoire01/vue-api-php/formulaire.php
+++ PHP-devoire01/vue-api-php/formulaire.php
@@ -14,23 +14,34 @@
 
         <h1>covoiturage</h1>
         <p>decription courte api covoiturage </h2>
-
+        <!--formulaire utilisent la methode POST. La sicle contenue dans  latribue action et lui meme : formulaire.php -->
         <form class="formCovoite" action="formulaire.php" method="post">
 
           <p> <label for="nom">écriver votre nom</label> </p>
-          <p> <input type="text" name="nom" value="" required pattern="[A-Za-z]+" placeholder="écriver votre nom ici" > </p>
+          <p> <input type="text" name="nom" value=""  placeholder="écriver votre nom ici" > </p>
+          <?php
+            if(!empty($nom))
+            {
+                echo ' votre nom est : ' . $nom . '<br>';
+              }
+              else
+              {
+              echo "merci de serire votre prenom";
+            }
+
+           ?>
 
           <p> <label for="prenom">écriver votre prénom</label> </p>
-          <p> <input type="text" name="prenom" value="" required pattern="[A-Za-z]+" placeholder="ecriver votre prenom ici" > </p>
+          <p> <input type="text" name="prenom" value=""  placeholder="ecriver votre prenom ici" > </p>
 
           <p> <label for="email">écriver votre email</label> </p>
           <p> <input type="email" name="email" value="" placeholder="ecriver vortre email ici" > </p>
 
           <p> <label for="numeroDeTelephone">écriver numérot de telephone a 10 chifre </label> </p>
-          <p> <input type="number" name="numeroDeTelephone" value="" required pattern="[0-9]+" placeholder="numéro de telephone"> </p>
+          <p> <input type="number" name="numeroDeTelephone" value=""  placeholder="numéro de telephone"> </p>
 
           <p> <label for="villeDeDepart">sectioner une ville de ville de depart</label> </p>
-        <!--  <p>   <input type="checkbox" name="villeDeDepart" value="villeDeDepart" placeholder="villeDeDepart" > -->
+        <!--  <p>   <input type="radio" name="villeDeDepart" value="villeDeDepart" placeholder="villeDeDepart" > -->
 
             <?php
               $villeDeDepart = array('Paris','Oreléans','Dublin','Nice','Tour' );
@@ -38,16 +49,13 @@
                 echo '<p>';
               for ($i=0; $i < 5 ; $i++) {
 
-                echo  $villeDeDepart[$i].  '<input type="checkbox" name= "villeDeDepart"  value='. $villeDeDepart[$i] .' > ';
+                echo  $villeDeDepart[$i].  '<input type="radio" name= "villeDeDepart"  value='. $villeDeDepart[$i] .' > ';
                 // code...
               }
               echo '</p>';
 
+
              ?>
-
-             <label for="villeDeDepart"> <select class="ville" name="villeDeDepart"> </label>
-
-             </select>
 
              <p> <label for="heureDeDepeart">indiquer une  heure de depeart <input type="time" name="heureDeDepeart" value="" > </label>  </p>
 
@@ -55,12 +63,13 @@
 
         </form>
 
+
+        <div class="resulta">
+
         <!-- teste et condition du formulaire -->
         <?php
 
         #  - je vais devoir verifier si tous les chant de formulaire son remplie par l'utilisateur avent validation
-
-
 
         #  var_dump($_POST);
 
@@ -77,15 +86,39 @@
          echo ' votre email : '. $email . ' votre tel : ' . $tel . '<br>';
          echo ' depart de : ' . $villeDepart .  '<br>';
          echo 'aficher heure sesie :' . $heure . '<br>';
-         echo 'validation : ' . $validation . '<br>';
+         echo 'validation : ' . $validation . '<br>_________<br>';
 
+         if(!empty($nom))
+         {
+               echo ' votre nom est : ' . $nom . '<br>';
+         }
+         else
+         {
+           echo "merci de serire votre prenom <br>";
+         }
+
+         if (isset ($villeDepart))
+         {
+           echo "vous aver choisie : '.$villeDepart.' comme ville de depart <br>" ;
+         }
+         else {
+           echo "vous devez choisir un ville de depart <br>";
+         }
+
+         if (!empty($heure))
+         {
+           echo "vous soueter partire a : '.$heure.'  <br>" ;
+         }
+         else {
+           echo "vous devez indiquer un heure de depart <br>";
+         }
 
          ?>
 
-      </main>
-
       <?php include("footer.php"); ?>
 
+      </div>
+    </main>
     </div>
 
   </body>
