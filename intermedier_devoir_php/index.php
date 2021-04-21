4da712caf3d43556ee3030c2122f9eac48e324de
david soutarson <triplaxs@gmail.com>
2021-04-21 19:00:07 +0200

    premier ider pour le devoire

---
+++ intermedier_devoir_php/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>suivi des recherches par internaute</title>
+  </head>
+  <body>
+    <div id="wraper">
+      <header>
+        <h1>Le site des villes</h1>
+        <nav>
+          <ul>
+            <li>acceuille</li>
+            <li>recherche</li>
+            <li>conextion
+        <ul>
+          <li><a href="vue/form_creer_cont.php">créer un cont</a></li>
+          <li> <a href="vue/form_conection.php">se conecter</a> </li>
+        </ul>
+      </li>
+            <li>Eta
+        <ul>
+          <li>Conecter</li>
+          <li>Non conecter</li>
+        </ul>
+      </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+
+        <?php include "vue/form_recherche.php"; ?>
+
+      </main>
+
+      <footer>
+        <p>David</p>
+        <p>Soutatson</p>
+        <p>Formation :Integrateur-developpeur</p>
+        <p>Eleve N°229855<p>
+        
+      </footer>
+
+    </div>
+
+  </body>
+</html>
---
+++ intermedier_devoir_php/vue/form_recherche.php
@@ -0,0 +1,9 @@
+<h3>Zone de recherche</h3>
+
+<form class="recherche villes" action="index.php" method="post">
+  <p><label for="recherche"> Ecriver Le nom de la vile Rechercher dans le champ ci dessous</label></p>
+  <p> <input type="text" name="recherche" value="" placeholder="Rechercher une ville"> </p>
+
+  <p> <label for="valider"></label> </p>
+  <input type="submit" name="valider" value="valider" >
+</form>
