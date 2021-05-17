24eef56a65e352a7ac882a8b4db383a357939c58
david soutarson <triplaxs@gmail.com>
2021-05-17 20:46:45 +0200

    2021 test php

--- revision/form02_anvoie.php
+++ revision/form02_anvoie.php
@@ -1,3 +1,6 @@
+<?php
+session_start(); //inisialisation de la session
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
@@ -7,38 +10,43 @@
   <body>
 
     <h1> Une revition sur les formulaire et leur tretemant </h1>
+    <?php
 
-    <form class="form_02" action="index.html" method="post">
+    if (isset( $_SESSION['errors']))
+    {
+        foreach ($_SESSION as $errors )
+        {
+          foreach ($errors as $name => $alerte)
+          {
+          echo $name. ' : ' . $alerte .'<br>';//afiche les ereurs a partir de la session créer danle fichier taitement
+          }
+        }
+    }
+
+
+
+    ?>
+    <form class="form_02" action="taitement.php" method="post">
       <input type="hidden" name="date" value="">
 
       <fieldset>
           <h2>formulaire d'authentification.</h2>
         <p>
           <label for="prenom"> Prenom :</label>
-          <input type="text" name="prenom" id=prenom value="" placeholder="ecriver votre Prenom ici." autocomplete="on" autofocus =" true" >
+          <input type="text" name="prenom" id="prenom" value="" placeholder="ecriver votre Prenom ici." autocomplete="on" autofocus =" true" >
         </p>
         <p>
           <label for="nom"> Nom :</label>
-          <input type="text" name="nom" id=nom value="" placeholder="ecriver votre Nom ici." autocomplete="on" autofocus =" true"  >
+          <input type="text" name="nom" id="nom" value="" placeholder="ecriver votre Nom ici." autocomplete="on" autofocus =" true"  >
         </p>
 
         <p>
           <label for="email">Email</label>
-          <input type="email" name="email" value="" placeholder="ecriver votre email ici." autocomplete="on" autofocus =" true" >
+          <input type="email" name="email" id="email" value="" placeholder="ecriver votre email ici." autocomplete="on" autofocus =" true" >
         </p>
 
         <p>
-          <label for="password">mots de passe</label>
-          <input type="password" name="password" value="" >
-        </p>
-
-        <p>
-          <label for="passe_verifier">verifier mot de passe</label>
-          <input type="password" name="passe_verifier" value="" >
-        </p>
-
-        <p>
-          <label for="condition_utilisater">Acepter vous que se cite conserve vaux recher et votre utilisateur sous forme de cookie <a href="#">en savoire plus</a> afin de fasiliter voutre experience utilisateur </label>
+          <label for="condition_utilisater">Acepter vous que se cite conserve vaux recher sous forme de setion <a href="#">en savoire plus</a> afin de fasiliter voutre experience utilisateur </label>
           <p>
             Oui jai lue est j'accepte <input type="radio" name="condition_utilisater" value="oui">
           </p>
@@ -58,6 +66,8 @@
 
     <p>1 verifier si le formulaire tautalement remplie </p>
     <p> 2 je fais remplie la base de doner avec les profile utisateur compler </p>
+
+
 
   </body>
 </html>
---
+++ revision/taitement.php
@@ -0,0 +1,39 @@
+<?php
+var_dump($_POST);
+
+$errors = [];
+
+if (empty($_POST['prenom'])) {
+  $errors['prenom']="Vous n'avez pas renseiger votre Prenon";
+}
+
+if (empty($_POST['nom'])) {
+  $errors['nom']="Vous n'avez pas renseiger votre Non" ;
+}
+
+if (empty($_POST['email'])) {
+  $errors['email']="Vous n'avez pas renseiger votre Email";
+}
+
+if (!isset($_POST['condition_utilisater'])) {
+  $errors['condition_utilisater']="choisier une condition d'utilisation";
+}
+
+if(!empty($errors))
+{
+  session_start(); //creation d'une session
+  $_SESSION['errors'] = $errors;
+  header('Location: form02_anvoie.php');
+}
+else {
+  // traitement de linformation
+  $prenom = $_POST['prenom'];
+  $nom = $_POST['nom'];
+  $email = $_POST['email'];
+
+  echo "<p> bonjour: " . $prenom . ", " .$nom. "</p>";
+
+}
+
+
+ ?>
