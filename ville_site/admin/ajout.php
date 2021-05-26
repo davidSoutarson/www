21d02f6b6ed0f5983042b07d9b82c4956b810ac9
david soutarson <triplaxs@gmail.com>
2021-05-27 00:44:13 +0200

    index.php & inc-conection & ville.php & ajout.php & inc_footer.php & inc_menue 1/3 ok

--- ville_site/admin/ajout.php
+++ ville_site/admin/ajout.php
@@ -11,19 +11,44 @@
 //récuperation des variables
 if (isset($_POST[submit_form])) // Avertissement: Utilisation de submit_form constantes non définies - supposé « submit_form » (cela lancera une erreur dans une future version de PHP) en E:\wamp64\www\ville_site\admin\ajout.php sur la ligne 12
 {
-  // code...
   $ville_nom = $_POST['ville_nom'];
   $ville_texte = $_POST['ville_texte'];
-  //requete inser into
-  if ($mysqli->query('INSERT INTO villes (ville_nom, ville_texte)
-  VALUES ("'.$ville_nom.'", "'.$ville_texte.'")'))
+  //verification du contenue des variables
+  if((empty($ville_nom)) OR empty($ville_texte))
   {
-/*si la requette et efectuer elle retourne le boolen TRUE et
-donc le mesage poura etre aficher*/
-    $message = '<p clase="message">L\'ajout de la ville'. $ville_nom .' est effectué. </p>';
+    $message = '<p class="error"> vous dever sesir le nom d\'une vile est sa présentation. </p>';
+  }
+  else
+  {
+    // ville exite dans la basse ?
+    // efetuon une requete SELECT avec count ()
+    $result = $mysqli->query('SELECT count(ville_id)
+    FROM villes WHERE ville_nom ="'. $ville_nom .'"');
+    $row = $result->fetch_array();
+    //$row[0] contient la valeur par count() de Mysql
+    if($row[0] > 0)
+    {
+      $message = '<p class="error"> La ville est déjat enregistrée. </p>';
+    }
+    else
+    {
+      // requete insert into
+      if($mysqli->query('INSERT INTO villes
+        (ville_nom, ville_texte)
+        VALUES
+        ("'. $ville_nom .'", "'. $ville_texte .'")'))
+        {
+          $message =  '<p class="message">Ajout de la ville: '.$ville_nom.' est efectuer </p>';
+        }
+        else
+        {
+          $message =  '<p class="error">Ajout de la ville: '.$ville_nom.' n\'est pas efectuer </p>';
+       }
+    }
   }
 }
      ?>
+
      <div>
        <h1>Ajout de ville</h1>
        <?php if(isset($message)) echo $message  ?>
