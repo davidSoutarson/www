e5a50f0aa74d26919995a4cad9d11d1fe25aebb0
david soutarson <triplaxs@gmail.com>
2021-08-07 22:46:28 +0200

    fin partie visiteure menbre

--- site_villes_devoir/fonction/inp_secur_fonc.php
+++ site_villes_devoir/fonction/inp_secur_fonc.php
@@ -7,6 +7,8 @@ $inp_user_loging = "";
 
 
 function securisation($donnees){
+  //$donnees =htmlspecialchars($donnees);
+  $donnees =htmlentities($donnees,ENT_NOQUOTES,"UTF-8");
   $donnees =trim($donnees);
   $donnees =strip_tags($donnees);
   return $donnees;
--- site_villes_devoir/index.php
+++ site_villes_devoir/index.php
@@ -1,10 +1,15 @@
 <?php
 session_start(); //inisialisation de la session
+if (!isset($_SESSION['compte_cree']) AND !isset($_SESSION['conectez']) ) {
+  header('location:conection.php');
+}
+
 require 'header.php';
 require 'menu.php';
 ?>
 
 <?php
+
 $requet='SELECT prenom,nom FROM user';
 $result = $mysqli->query($requet);
 
@@ -22,24 +27,29 @@ $result = $mysqli->query($requet);
 
   <article class="">
 
-    <p> Bomjour et bien venue sur le site des ville . Bonne visite :</p>
-
-    <p> Salutation personaliser !!!</p>
-
-    <p> Proposion de recher personaliser !!!</p>
+    <p> Bien venue sur le site des ville . Bonne visite :</p>
 
 
       <?php
-         echo $_SESSION['compte_cree'];
-         echo"<p> Bomjour :".$_SESSION['compt_nom'] ."</p>";
-         echo"<p> pseudo utilisateure :". $_SESSION['compt_pseudo'] ."</p>";
-         echo'<p> votre email :' .$_SESSION['compt_maile'] .'</p>';
-         
-         var_dump($_SESSION)
+
+
+      if ($_SESSION ) {
+
+          if (isset($_SESSION['compte_cree']) ) {
+            echo $_SESSION['compte_cree']. "" .$_SESSION['compt_nom'] ."".$_SESSION['compt_pseudo'] ."".$_SESSION['compt_maile'] ;
+
+          }
+
+          if (isset($_SESSION['conectez']) ) {
+            echo $_SESSION['conectez']."" .$_SESSION['conect_pseudo'] ."".$_SESSION['conect_loging'] ;
+
+          }
+
+         //var_dump($_SESSION);
+      }
+
       ?>
 
   </article>
-
-
 
 <?php require 'footeur.php';?>
