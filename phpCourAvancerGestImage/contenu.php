c9f1a151302b3dd29f34561748879015b2854f7a
david soutarson <triplaxs@gmail.com>
2022-04-21 12:03:39 +0200

    test v01 cour php poo gertion image

---
+++ phpCourAvancerGestImage/contenu.php
@@ -0,0 +1,10 @@
+<?php
+require ('class/image.php');
+$image = new Image ();
+$images = $image->getImages(IMAGE_DIR_PATH);
+?>
+<ul>
+  <?php foreach ($images as $image):?>
+    <li><img src="<?php echo IMAGE_DIR_URL . image ?>"/></li>
+  <?php endforeach ?>
+</ul>
